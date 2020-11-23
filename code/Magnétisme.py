725fbfda2cf869c914860582dcc03f6fd8ce43fe
grybouilli <grybouilli@outlook.fr>
2020-11-23 18:28:06 +0100

    produits vectoriel et scalaire

--- code/Magnétisme.py
+++ code/Magnétisme.py
@@ -15,22 +15,36 @@ mu0 = 1.25663706 * 10**(-6)
 print(mu0)
 
 class Vecteur:
-    def __init__ (self,r,theta,z):
-        self.r = r
-        self.theta = theta
+    def __init__ (self,x,y,z):
+        self.x = x
+        self.y = y
         self.z = z
 
-    def get_r(self):
-        return(self.r)
-    def get_theta(self):
-        return(self.theta)
+    def get_x(self):
+        return(self.x)
+    def get_y(self):
+        return(self.y)
     def get_z(self):
         return(self.z)
 
-    def set_r(self,r1):
-        self.r = r1
-    def set_theta(self,theta1):
-        self.theta = theta1
+    def set_x(self,x1):
+        self.x = x1
+    def set_y(self,y1):
+        self.y = y1
     def set_z(self,z1):
         self.z = z1
+
+def prod_scal(vect1,vect2):
+    (x1,x2) = (vect1.get_x(),vect2.get_x())
+    (y1,y2) = (vect1.get_y(),vect2.get_y())
+    (z1,z2) = (vect1.get_z(),vect2.get_z())
+    return x1*x2 + y1*y2 + z1*z2
+    
+def prod_vec(vec1, vec2):
+    (x1,x2) = vec1.get_x(), vec2.get_x()
+    (y1,y2) = vec1.get_y(), vec2.get_y()
+    (z1,z2) = vec1.get_z(), vec2.get_z()
+
+    return Vecteur(y1*z2-y2*z1, z1*x2-x1*z2, y2*x1-y1*x2)
+    
 
