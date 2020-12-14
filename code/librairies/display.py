911bee4ce1ffa61236054fdcadb0217e64f2f9f3
grybouilli <grybouilli@outlook.fr>
2020-12-14 17:03:05 +0100

    fichier très utile pour matplotlib

---
+++ code/librairies/display.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+def afficher(x,y,name):
+    plt.figure(name)
+    plt.plot(x,y)
+    plt.show()
+
+def afficher_plusieurs(x,ys,names):
+    plt.figure("graphe")
+    for y in range(len(ys)):
+        plt.plot(x, ys[y], label=names[y])
+    plt.legend()
+    plt.show()
