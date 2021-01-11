e8e908c8d5bf159807e8e6422d1a503c1fe2c7f1
grybouilli <grybouilli@outlook.fr>
2021-01-11 17:17:18 +0100

    fucking fix everything

--- code/Magnétisme.py
+++ code/Magnétisme.py
@@ -20,10 +20,7 @@ mu0 = 1.25663706 * 10**(-6)
 
 #Pour r qui se rapproche de 0:
 # Calcule la composante du champ magnétique selon r à un instant donné
-def Br0(vec):
-
-    pol = polar_of_cart(vec)
-    r = pol.get_x()
+def Br0(r, z):
     a = mu0 * n * i / 4
     xip = z + L/2
     xim = z - L/2
@@ -35,7 +32,7 @@ def Br0(vec):
 
 
 # Calcule la composante du champ magnétique selon z à un instant donné
-def Bz0(vec):
+def Bz0(r, z):
 
     a = mu0 * n * i / 2
     xip = z + L/2
@@ -46,8 +43,7 @@ def Bz0(vec):
     
     return a * (f(xip) - f(xim))
 
-#cas général
-def Br
+
 
 #Renvoie le vecteur champ magnétique à un instant donné
 def calc_magn1(vec):
--- code/etude_champs_magn.py
+++ code/etude_champs_magn.py
@@ -3,7 +3,7 @@ import librairies.display as disp
 import matplotlib.pyplot as plt
 import numpy as np
 
-#toutes les distances en m
+# Pour r qui se raproche de 0
 n = 1000
 fig1 = plt.figure()
 fig2 = plt.figure()
@@ -13,13 +13,12 @@ ax2 = fig2.gca(projection='3d')
 r = np.linspace(0, 50, n)
 z = np.linspace(0, 50, n)
 r, z = np.meshgrid(r, z)
-coordBr = mg.Br(r, z) 
-coordBz = mg.Bz(r, z) 
+coordBr = mg.Br0(r, z) 
+coordBz = mg.Bz0(r, z) 
 
 
 planeBr = ax1.plot_surface(r, z, coordBr, label = 'Br', color = 'b')
 planeBz = ax2.plot_surface(r, z, coordBz, label = 'Bz', color = 'r') 
 
-fig1.colorbar(planeBr, aspect = 5)
-fig2.colorbar(planeBz, aspect = 5)
+
 plt.show()
