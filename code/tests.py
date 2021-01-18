5ea0f35a8f758df1f5ed811736ce198c27af2c2c
grybouilli <grybouilli@outlook.fr>
2021-01-18 17:48:07 +0100

    rebut

--- code/tests.py
+++ code/tests.py
@@ -1,6 +1,39 @@
 from Magnétisme import *
 from utilities import *
+import librairies.display as disp
+import matplotlib.pyplot as plt
+import numpy as np
 
-test = Vecteur(2, 0, 2)
-print(K_int(0.5))
-print(E_int(0.5))
+
+def K_int(k):
+    (n,m) = len(k), len(k[0])
+    K= np.zeros(len(k))
+    for i in range(n):
+        for j in  range(m):
+            K[i] = ellipk([k[i][j]])
+    return K
+
+def E_int(k):
+    (n,m) = len(k), len(k[0])
+    E = np.zeros(len(k))
+    for i in range(n):
+        for j in range(m):
+            E[i] = ellipe([k[i][j]])
+    return E
+
+def K_int2(phi, k):
+    (n,m) = len(k), len(k[0])
+    K= np.zeros(len(k))
+    for i in range(n):
+        for j in range(m):
+            K[i] = ellipkinc([phi[i][j]],[k[i][j]])
+    return K
+
+#Complete ellipitc integral, second kind : E(k) = E(pi/2, k)
+def E_int2(phi, k):
+    (n,m) = len(k), len(k[0])
+    E = np.zeros(len(k))
+    for i in range(n):
+        for j in range(m):
+            E[i] = ellipeinc([phi[i][j]],[k[i][j]])
+    return E
