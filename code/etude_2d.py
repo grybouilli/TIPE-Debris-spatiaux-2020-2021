36d3823fa1267d29335a2997f18fd2dbf686f666
Maxime <maxime.wirth88@orange.fr>
2021-02-01 16:40:17 +0100

    pour toi Nico

--- code/etude_2d.py
+++ code/etude_2d.py
@@ -3,3 +3,18 @@ import librairies.display as disp
 import matplotlib.pyplot as plt
 import numpy as np
 
+fig1 = plt.figure()
+fig2 = plt.figure()
+fig3 = plt.figure()
+
+ax1 = fig1.gca(projection='3d')
+ax2 = fig2.gca(projection='3d')
+ax3 = fig3.gca(projection='3d')
+
+r = np.linspace(1, 11, n)
+z = np.linspace(1, 15, n)
+r, z = np.meshgrid(r, z)
+
+coordBr = mg.Br(r, z)
+coordBz = mg.Bz(r, z)
+coordNorm = mg.normeB(r, z)
