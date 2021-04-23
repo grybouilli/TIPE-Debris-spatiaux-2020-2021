49a7fc795ec8eef1dc548841921b4762aca86f1f
Maxime <maxime.wirth88@orange.fr>
2021-04-23 12:02:45 +0200

    def de Br

--- code/force_magnetique.py
+++ code/force_magnetique.py
@@ -13,3 +13,21 @@ def dk_dr(r, xi):
     prem = mg.a / (np.sqrt((mg.a * r)**2 + xi**2 ) * np.sqrt(mg.a * r))
     deuz = 2*np.sqrt(mg.a * r) * (mg.a + r) / ( (mg.a + r)**2 + xi**2 )**(-3/2)
     return prem - deuz
+
+def xip(z):
+    return z + mg.L/2
+
+def xim(z):
+    return z - mg.L/2
+
+def tBr(z, r):
+    xiplus = xip(z)
+    ximo = xim(z)
+    A = mg.mu0 * mg.n * mg.i / np.pi
+    B = np.sqrt(mg.a / r)
+    def func(r, xi):
+        prem = (2 - petit_k(r, xi**2))/(2*petit_k(r, xi)) * defi.approx_K(petit_k(r, xi))
+        dez = defi.approx_E(petit_k(r, xi))/petit_k(r, xi)
+        return prem - dez
+    return A * B * (func(r, xiplus) - func(r, ximo))
+
