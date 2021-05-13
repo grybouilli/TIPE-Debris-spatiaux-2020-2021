2653b6fa65d2893fe591cfb81347d3f756e77eaa
Maxime <maxime.wirth88@orange.fr>
2021-05-13 14:29:00 +0200

    vect_direct + rotation

--- code/aimantation.py
+++ code/aimantation.py
@@ -18,7 +18,6 @@ e = 1.602 * 10**(-19) #C
 #Magnéton de Bohr
 MU_B = e * H_BARRE / (2 * m_e) #J/T
 
-
 def E_Fermi(n):
     a = (3 * np.pi*np.pi * n) ** (2/3)
     return H_BARRE**2 * a / (2 * m_e)
@@ -26,6 +25,17 @@ def E_Fermi(n):
 EF_NBTI = E_Fermi(n_NBTI) #J
 
 def norme_M(r, z):
-    B = mg.normeB(r,z)
+    B = mg.normeB(r, z)
     return 3 * MU_B**2 * n_NBTI * B / (2*EF_NBTI)
 
+def vect_direct(r, z):
+    a = mg.normeB(r, z)
+    return (mg.Br(r, z)/ a, mg.Bz(r, z)/a)
+
+def rotation(alpha, vect):
+    a = np.cos(alpha)
+    b = np.sin(alpha)
+    (x, y) = vect
+    xx = x*a - y*b
+    yy = x*b + y*a
+    return (xx, yy)
