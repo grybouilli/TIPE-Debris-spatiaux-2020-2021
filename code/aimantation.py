1c5078d931c9a044262c2c49d6ec6124b2dd5342
Maxime <maxime.wirth88@orange.fr>
2021-05-13 11:49:44 +0200

    norme M

--- code/aimantation.py
+++ code/aimantation.py
@@ -3,9 +3,20 @@ import numpy as np
 
 #Définitions des constantes
 
+#Constante de Planck réduite
 H_BARRE = 1.054 * 10**(-34) #J.S
+
+#Masse d'un électron
 m_e = 9.11 * 10**(-31) #kg
+
+#Densité en électron du Nb-Ti
 n_NBTI = 1.25 * 10**29 #électrons/m^3
+
+#Charge élémentaire
+e = 1.602 * 10**(-19) #C
+
+#Magnéton de Bohr
+MU_B = e * H_BARRE / (2 * m_e) #J/T
 
 
 def E_Fermi(n):
@@ -14,4 +25,7 @@ def E_Fermi(n):
 
 EF_NBTI = E_Fermi(n_NBTI) #J
 
+def norme_M(r, z):
+    B = mg.normeB(r,z)
+    return 3 * MU_B**2 * n_NBTI * B / (2*EF_NBTI)
 
