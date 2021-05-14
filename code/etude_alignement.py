5c5081dcede4c1c9f923349811f3ea144cb32846
grybouilli <grybouilli@outlook.fr>
2021-05-14 14:32:29 +0200

    pas fini

--- code/etude_alignement.py
+++ code/etude_alignement.py
@@ -1,9 +1,26 @@
-from Magnétisme import *
+import Magnétisme as mg
+import aimantation as aim
 import matplotlib.pyplot as plt
 import numpy as np
 
+m = 10
+h = 5
+l = 5
+L = 5
 def angle_alpha_moment(t,r,z,p,J_theta,alpha0):
-    bz = Bz(r,z)
-    br = Br(r,z)
-    pulsation = np.sqrt(p*Bz) / J_theta
+    bz = mg.Bz(r,z)
+    br = mg.Br(r,z)
+    pulsation = np.sqrt(p*bz) / J_theta
     return (alpha0 - br/bz) * np.cos(pulsation * t) + br/bz
+
+r = 100
+z = 0
+moment_p = aim.norme_M(r,z)
+J = m*(h**2+l**2+L**2) / 12
+
+t = np.linspace(0,5, 1000)
+alpha = angle_alpha_moment(t,r,z,moment_p,J, 0)
+
+plt.figure()
+plt.plot(t,alpha)
+plt.show()
