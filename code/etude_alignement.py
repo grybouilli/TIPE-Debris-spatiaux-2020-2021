67fc925a00b12280ed6da6bc451f3f77e29b13e3
grybouilli <grybouilli@outlook.fr>
2021-05-14 15:29:25 +0200

    up up

--- code/etude_alignement.py
+++ code/etude_alignement.py
@@ -11,16 +11,34 @@ def angle_alpha_moment(t,r,z,p,J_theta,alpha0):
     bz = mg.Bz(r,z)
     br = mg.Br(r,z)
     pulsation = np.sqrt(p*bz) / J_theta
-    return (alpha0 - br/bz) * np.cos(pulsation * t) + br/bz
+
+    alpha_thau = np.arccos(1/(alpha0*(bz/br) - 1))
+    return (alpha0 - br/bz) * np.cos(pulsation * t) + br/bz , alpha_thau/pulsation
 
 r = 100
-z = 0
+z = 1
 moment_p = aim.norme_M(r,z)
 J = m*(h**2+l**2+L**2) / 12
 
-t = np.linspace(0,5, 1000)
-alpha = angle_alpha_moment(t,r,z,moment_p,J, 0)
+t = np.linspace(0,1000, 10000)
+alpha_0 = [np.pi/3, np.pi/4, np.pi/6]
+alpha = []
+thau = []
 
+for a in alpha_0:
+    al,th =angle_alpha_moment(t,r,z,moment_p,J,a)
+    alpha.append(al)
+    thau.append(th)
 plt.figure()
-plt.plot(t,alpha)
+plt.plot(t,alpha[0], label='alpha_0 = pi/3')
+plt.plot(t,alpha[1], label='alpha_0 = pi/4')
+plt.plot(t,alpha[2], label='alpha_0 = pi/6')
+
+alpha_0_max = max(alpha_0)
+for t in thau:
+    plt.vlines(t, - alpha_0_max, alpha_0_max)
+
+plt.xlabel('time (s)')
+plt.ylabel('alpha (rad)')
+plt.legend()
 plt.show()
