85d515aa6646331351c405ffea545e28f4ae3811
grybouilli <grybouilli@outlook.fr>
2021-05-18 21:57:06 +0200

    ajout du solénoïde aux graphes

--- code/etude_champs_magn.py
+++ code/etude_champs_magn.py
@@ -34,9 +34,35 @@ coordNorm = mg.normeB(abs(r), abs(z))
 '''
 planeBr0 = ax1.plot_surface(r, z, coordBr0, label = 'Br0', color = 'b')
 planeBz0 = ax2.plot_surface(r, z, coordBz0, label = 'Bz0', color = 'r') '''
-planeBr = ax3.plot_surface(r, z, coordBr, label = 'Br', color = 'g')
-planeBz = ax4.plot_surface(r, z, coordBz, label = 'Bz', color = 'm')
-planeNorm = ax5.plot_surface(r, z, coordNorm, label = 'Norme de B', color = 'y')
+planeBr = ax3.plot_surface(r, z, coordBr, label = 'Br', color = 'g',alpha=0.5)
+planeBz = ax4.plot_surface(r, z, coordBz, label = 'Bz', color = 'm',alpha=0.5)
+planeNorm = ax5.plot_surface(r, z, coordNorm, label = 'Norme de B', color = 'y',  alpha=0.5)
 
+#étiquettes
+
+ax3.set_xlabel("r")
+ax3.set_ylabel("z")
+ax3.set_zlabel("Br")
+
+ax4.set_xlabel("r")
+ax4.set_ylabel("z")
+ax4.set_zlabel("Bz")
+
+ax5.set_xlabel("r")
+ax5.set_ylabel("z")
+ax5.set_zlabel("B")
+
+# Solénoide
+z_sol=np.linspace(-mg.L/2, mg.L/2,n)
+
+theta = np.linspace(0, 2*np.pi, n)
+theta_grid, z_grid=np.meshgrid(theta, z_sol)
+x_grid = mg.a*np.cos(theta_grid) 
+y_grid = mg.a*np.sin(theta_grid) * 35
+rstride = 20
+cstride = 10
+ax3.plot_surface(x_grid, z_grid, y_grid)
+ax4.plot_surface(x_grid, z_grid, y_grid)
+ax5.plot_surface(x_grid, z_grid, y_grid)
 plt.show()
 #Streamplot et quiver pour les cartes et lignes de champ
