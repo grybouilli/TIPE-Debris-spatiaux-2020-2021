bf7b77b6045deac5d8ac76815650f4db0e5e7357
R-0-M-1 <romainmaurice7@gmail.com>
2021-05-20 18:58:59 +0200

    mv par verlet et pas adptatif

--- code/Mouvement_tmp.py
+++ code/Mouvement_tmp.py
@@ -8,34 +8,62 @@ def accél (m, r,z,t, alpha0):
     f_z *= (1/m)
     return (f_r, f_z)
 
-def position(m, alpha0, p0, tf, n):
+def position_euler(m, alpha0, p0, tf, n):
     h = tf/n #pas
-    Y = np.zeros((2, n)) #vitesse initiale
+    V = np.zeros((2, n)) #vitesse initiale
     P = np.zeros((2, n))
     P[0][0], P[1][0] = p0
     T = np.zeros(n)
     for k in range (1, n):
         a_r , a_z = accél(m, P[0][k-1], P[1][k-1], T[k-1], alpha0)
-        Y[0][k] = Y[0][k-1] + h*a_r
-        Y[1][k] = Y[1][k-1] + h*a_z
-        P[0][k] = P[0][k-1] + h*Y[0][k-1] #je sais pas si c'est k ou k-1 dans le Y, à vérifier
-        P[1][k] = P[1][k-1] + h*Y[1][k-1]
+        V[0][k] = V[0][k-1] + h*a_r
+        V[1][k] = V[1][k-1] + h*a_z
+        P[0][k] = P[0][k-1] + h*V[0][k-1] #je sais pas si c'est k ou k-1 dans le Y, à vérifier
+        P[1][k] = P[1][k-1] + h*V[1][k-1]
         T[k] = k*h
-    return (P[0], P[1], T)
+    return (P[0], P[1])
 
+def position_verlet_var(m, alpha0, p0, tf, delta_pos):
+    V_r = np.zeros(1)
+    V_z = np.zeros(1)
+    P_r = np.zeros(1)
+    P_z = np.zeros(1)
+    P_r[0], P_z[0] = p0
+    
+    t = 0
+    k = 0
+    h = 0.01
+    
+    while t < tf :
+        a_r_n, a_z_n = accél(m, P_r[k], P_z[k], t, alpha0)
+        P_r = np.append(P_r, P_r[k] + h*V_r[k] + ((h**2)/2)*a_r_n)
+        P_z = np.append(P_z, P_z[k] + h*V_z[k] + ((h**2)/2)*a_z_n)
+        t += h
+#        print("t =", t)        
+        
+        a_r_n1, a_z_n1 = accél(m, P_r[k+1], P_z[k+1], t, alpha0)
+        V_r = np.append(V_r, V_r[k] + (h/2)*(a_r_n + a_r_n1))
+        V_z = np.append(V_z, V_z[k] + (h/2)*(a_z_n + a_z_n1))
+        
+        v = np.sqrt((V_r[k+1])**2 + (V_z[k+1])**2)
+#        print("v=", v)
+        if v > (delta_pos/tf)*1000 :
+            h = delta_pos/v
+        else :
+            h = tf/100
+#        print("h =", h)
+        k += 1
+    return (P_r, P_z, t)
 
-pos_r, pos_z, T = position(0.1, 0.1, (0.5, 3), 0.01, 10000)
-print(pos_r)
-print(pos_z)
+pos_r, pos_z, t_fin = position_verlet_var(0.1, 0, (0.5, 2), 1000, 0.01)
+#pos_r, pos_z = position_euler(0.1, 0.1, (0.5, 3), 100, 10000)
+print("t =", t_fin)
 
-plt.plot(pos_r, pos_z)
+plt.plot(pos_r, pos_z, color = 'r')
 plt.show()
 
 '''
-pas de temps addaptatif
-
-améliorer euler
- --> verlet par ex
+pas de temps adaptatif
 
 carte de champ // lignes de champ ??
 '''
