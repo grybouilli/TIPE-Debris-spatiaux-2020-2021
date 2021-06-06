1e5eba09e3fe43736b2842758eeafda2b18a79ab
grybouilli <grybouilli@outlook.fr>
2021-06-06 15:53:27 +0200

    début étude

---
+++ code/populations_mouvements.py
@@ -0,0 +1,8 @@
+import mouvement as mvt
+import matplotlib.pyplot as plt
+import numpy as np
+import operateurs_vectoriels as op
+import champ_mag as mag
+
+
+#première phase de test pour savoir quoi étudier exactemet et comment présenter les résultats
